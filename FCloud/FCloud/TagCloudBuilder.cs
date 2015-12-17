782c5eec73d1129f7afdaf962b60524a9b3c7e0f
Ilya Kuchumov <sdp.kuchumov-ilya@yandex.ru>
2015-12-17 22:05:47 +0500

    add TagCloudBuilder

---
+++ FCloud/FCloud/TagCloudBuilder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+
+namespace FCloud
+{
+    public class TagCloudBuilder
+    {
+        public Bitmap Build(string text,
+            Func<string, IEnumerable<string>> parseWords,
+            Func<string, string> normalizeWord,
+            Func<string, bool> isInterestingWord,
+            Func<IEnumerable<string>, Bitmap> drawOnBitmap)
+        {
+            var words = parseWords(text)
+                .Select(normalizeWord)
+                .Where(isInterestingWord);
+            return drawOnBitmap(words);
+        }
+    }
+}
