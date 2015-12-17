87e310120d3e3e59b8fcd0eaf1b24957a9614b73
Ilya Kuchumov <sdp.kuchumov-ilya@yandex.ru>
2015-12-18 01:33:52 +0500

    Add ImageBitmapWriter

---
+++ FCloud/FCloud/DataWriting/ImageBitmapWriter.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace FCloud.DataWriting
+{
+    public class ImageBitmapWriter
+    {
+        public void WriteBitmap(Bitmap bitmap, Options options)
+        {
+            var toImageFormatConverter = new Dictionary<string, ImageFormat>
+            {
+                {".png", ImageFormat.Png},
+                {".jpeg", ImageFormat.Jpeg},
+                {".bmp", ImageFormat.Bmp}
+            };
+            bitmap.Save(options.OutputFilePath + options.OutputFormat, toImageFormatConverter[options.OutputFormat]);
+        }
+    }
+}
