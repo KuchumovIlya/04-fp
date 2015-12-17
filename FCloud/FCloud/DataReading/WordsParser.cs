f63253c4fb87999883573c468ec1ce574152d5b7
Ilya Kuchumov <sdp.kuchumov-ilya@yandex.ru>
2015-12-17 22:37:24 +0500

    add wordsParser with tests

---
+++ FCloud/FCloud/DataReading/WordsParser.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace FCloud.DataReading
+{
+    public class WordsParser
+    {
+        public IEnumerable<string> ParseWords(string text)
+        {
+            var regExp = new Regex(@"\W|\d");
+            return regExp
+                .Split(text.ToLower())
+                .Where(s => s != "");
+        }
+    }
+}
---
+++ FCloud/FCloudTests/WordsParser_Should.cs
@@ -0,0 +1,57 @@
+﻿using FCloud.DataReading;
+using NUnit.Framework;
+
+namespace FCloudTests
+{
+    [TestFixture]
+    public class WordsParser_Should
+    {
+        [Test]
+        public void SplitTextOnLettersTokens()
+        {
+            const string text = "ab аб cd";
+            var parser = new WordsParser();
+            var expected = new[] {"ab", "аб", "cd"};
+            
+            var actual = parser.ParseWords(text);
+        
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void SkipDigitsTokens()
+        {
+            const string text = "a 123 b";
+            var parser = new WordsParser();
+            var expected = new[] { "a", "b" };
+
+            var actual = parser.ParseWords(text);
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void SkipEmptyWords()
+        {
+            const string text = "   ab    аб    cd    ";
+            var parser = new WordsParser();
+            var expected = new[] { "ab", "аб", "cd" };
+
+            var actual = parser.ParseWords(text);
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void ChangeLettersToLowercase()
+        {
+            const string text = "ААББCCZZ";
+            var parser = new WordsParser();
+            var expected = new[] { "ааббcczz" };
+
+            var actual = parser.ParseWords(text);
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+    }
+}
