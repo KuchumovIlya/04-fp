d711e1cd8f111c6fa815b93e9a42579401e8b582
Ilya Kuchumov <sdp.kuchumov-ilya@yandex.ru>
2015-12-17 23:07:07 +0500

    add MystemReader with tests

---
+++ FCloud/FCloud/DataReading/MystemReader.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace FCloud.DataReading
+{
+    public class MystemReader : IDisposable
+    {
+        private readonly Process process;
+
+        public MystemReader(string pathToMystem)
+        {
+            var processStartInfo = new ProcessStartInfo
+            {
+                Arguments = "-nig --format json -e utf-8",
+                RedirectStandardOutput = true,
+                RedirectStandardInput = true,
+                UseShellExecute = false,
+                FileName = pathToMystem
+            };
+            process = new Process { StartInfo = processStartInfo };
+            process.Start();
+        }
+
+        private static bool IsRussianLetter(char c)
+        {
+            return Regex.IsMatch(c.ToString(), @"\p{IsCyrillic}");
+        }
+
+        public string ReadJson(string word)
+        {
+            if (!word.All(IsRussianLetter))
+                return string.Format("{{\"analysis\":[],\"text\":\"{0}\"}}", word);
+            WriteText(word);
+            return ReadText();
+        }
+
+        private void WriteText(string text)
+        {
+            var textInBytes = Encoding.UTF8.GetBytes(text + "\n");
+            process.StandardInput.BaseStream.Write(textInBytes, 0, textInBytes.Length);
+            process.StandardInput.Flush();
+        }
+
+        private string ReadText()
+        {
+            var readedBytes = new List<byte>();
+            while (true)
+            {
+                var readedByte = process.StandardOutput.BaseStream.ReadByte();
+                const byte nextLineCharacter = 13;
+                if (readedByte == nextLineCharacter)
+                {
+                    process.StandardOutput.BaseStream.ReadByte();
+                    break;
+                }
+                readedBytes.Add((byte)readedByte);
+            }
+            return Encoding.UTF8.GetString(readedBytes.ToArray());
+        }
+
+        public void Dispose()
+        {
+            process.Close();
+        }
+    }
+}
---
+++ FCloud/FCloudTests/MystemReader_Should.cs
@@ -0,0 +1,47 @@
+﻿using FCloud.DataReading;
+using NUnit.Framework;
+
+namespace FCloudTests
+{
+    [TestFixture]
+    public class MystemReader_Should
+    {
+        const string PathToMystem = "../../mystem.exe";
+
+        [Test]
+        public void ReadDataForRussianWords()
+        {
+            const string word = "бежать";
+            const string expected = "{\"analysis\":[{\"lex\":\"бежать\",\"gr\":\"V,нп=(инф,несов|инф,сов)\"}],\"text\":\"бежать\"}";
+            using (var parser = new MystemReader(PathToMystem))
+            {
+                var actual = parser.ReadJson(word);
+                Assert.AreEqual(expected, actual);
+            }
+        }
+
+        [Test]
+        public void ReadEmptyDataForDigits()
+        {
+            const string word = "1234";
+            const string expected = "{\"analysis\":[],\"text\":\"1234\"}";
+            using (var parser = new MystemReader(PathToMystem))
+            {
+                var actual = parser.ReadJson(word);
+                Assert.AreEqual(expected, actual);
+            }
+        }
+
+        [Test]
+        public void ReadEmptyDataForEnglishWords()
+        {
+            const string word = "home";
+            const string expected = "{\"analysis\":[],\"text\":\"home\"}";
+            using (var parser = new MystemReader(PathToMystem))
+            {
+                var actual = parser.ReadJson(word);
+                Assert.AreEqual(expected, actual);
+            }
+        }
+    }
+}
